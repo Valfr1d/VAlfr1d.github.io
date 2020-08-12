ee3ae5809c3eba2478f5e04f04a8845897a4f0f5
Valfr1d <justus.walden@edu.tampere.fi>
2020-08-12 14:15:53 +0300

    12.8 harjoitus

---
+++ demo1.js
@@ -0,0 +1,53 @@
+console.log("Kohta A")
+/* Kohta A */
+let admin;
+let nimi; //muuttujan esittely
+nimi = "Juha";//muuttujalle arvo
+admin = nimi;//muuttujan arvo sijoitetaan toiselle muuttujalle
+
+console.log(admin);//tulostetaan konsoliin muuttujan admin arvo
+
+console.log("Kohta B")
+/* Kohta B */
+let planeetta;
+let quest;
+
+console.log("Kohta C")
+/* Kohta C */
+let sade;
+let piiri;
+let ala;
+let halkaisija = 3;
+
+sade = halkaisija/2;
+piiri = 2 * Math.PI * sade;
+ala = Math.PI * sade * sade;
+
+console.log("Ympyrän säde on " + sade + ", piiri on " + piiri + " ja pinta-ala on " + ala);
+
+
+console.log("Kohta D");
+/* Kohta D*/
+let matka = 370;
+let matkanhinta;
+let bensanhinta = 1.56;
+let kulutus = 5.5
+
+matkanhinta = kulutus * matka/100 * bensanhinta;
+/* bensaa kuluu 5.5*370/100 ja se maksaa kulutus * bensanhinta */
+
+console.log("Matka maksaa " + matkanhinta + "euroa ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ harjoitus1.js
@@ -0,0 +1,40 @@
+/* Kohta A + B */
+let nimi;
+nimi = "Full-stack koodari";
+
+console.log(nimi + " hei ");
+
+/* Kohta C */
+
+let kaverin_nimi;
+
+kaverin_nimi = "Jerry";
+
+console.log(" Justus ja " + kaverin_nimi + " ovat kavereita")
+
+/* Kohta D */
+
+let summa
+
+summa = 5+2
+
+console.log(summa)
+
+/* Kohta E */
+
+console.log(15+summa)
+
+/* Kohta F*/
+
+muuttuja_1 = 32
+muuttuja_2 = 67
+
+console.log(muuttuja_1 + muuttuja_2 + " Summa")
+// Laskee summman //
+console.log(muuttuja_1 - muuttuja_2 + " Erotus")
+// Laskee erotuksen //
+console.log(muuttuja_1 * muuttuja_2 + " tulo")
+// laskee tulon //
+console.log(muuttuja_1 / muuttuja_2 + " osamäärä")
+// laskee osamäärän //
+
